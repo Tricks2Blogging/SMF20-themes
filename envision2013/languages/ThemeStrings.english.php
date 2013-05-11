be8ce2cd1072dd27b627c0989a6fc960c5b7f948
Bjorn Kristiansen <colby.underhill@gmail.com>
2013-05-11 18:07:31 +0200

    Added Envision2013 theme.

---
+++ envision2013/languages/ThemeStrings.english.php
@@ -0,0 +1,14 @@
+<?php
+  
+$txt['bloc_replies'] = 'Replies';
+$txt['bloc_unread'] = 'Unread';
+
+$txt['bloc_approvals'] = 'Unapproved';
+$txt['bloc_reports'] = 'Reports';
+$txt['bloc_followus'] = 'Follow us on Twitter';
+$txt['bloc_fromtwitter'] = 'From Twitter';
+$txt['bloc_custom'] = 'Custom theme options';
+
+$txt['bloc_maintenace'] = 'Maintenace';
+
+?>
